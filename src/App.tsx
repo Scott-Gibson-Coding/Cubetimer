9f9ac30b348eac31ec5ecd3d99e3114b9e1c046f
Scott Gibson <scottchgibson@gmail.com>
2024-01-27 11:10:11 -0800

    Setup initial react-ts project using Vite

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function Cubetimer() {
+  return (
+    <div
+      style={{
+        fontSize: "48px",
+        fontWeight: "bold",
+        textAlign: "center",
+        padding: "2rem",
+      }}
+    >
+      Full Reset: Nothing to see here
+    </div>
+  );
+}
+
+export default Cubetimer;
